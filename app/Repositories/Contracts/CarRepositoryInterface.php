fb867ba0a4cf6790f90898850d2ae109cb9948f4
MayssaTaki <maessataki@gmail.com>
2025-05-27 13:59:13 +0300

    fix: car && id

--- app/Repositories/Contracts/CarRepositoryInterface.php
+++ app/Repositories/Contracts/CarRepositoryInterface.php
@@ -11,6 +11,7 @@ interface CarRepositoryInterface
     public function findById(int $id): ?Car;
     public function update(Car $car, array $data): Car;
     public function countCars(): int;
+    public function isCarBook(int $carId): bool;
     public function updateStatus(int $carId, string $status): bool;
  public function isCarAvailable(int $carId): bool;
   public function findWithLock(int $id);
